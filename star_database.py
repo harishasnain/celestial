901e3fd5d1b8e84690ae626ccebc069f4fb4041c
Haris <harishasnain009@gmail.com>
2024-10-15 14:10:58 -0400

    should be able to run now

--- star_database.py
+++ star_database.py
@@ -1,32 +1,41 @@
 import sqlite3
 
 class StarDatabase:
-    def __init__(self, db_path='stars.db'):
-        self.conn = sqlite3.connect(db_path)
-        self.cursor = self.conn.cursor()
-        self._create_table()
+    def __init__(self, file_path='bsc5.dat'):
+        self.stars = self._load_stars(file_path)
 
-    def _create_table(self):
-        self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS stars
-            (id INTEGER PRIMARY KEY, name TEXT, ra REAL, dec REAL, magnitude REAL)
-        ''')
-        self.conn.commit()
+    def _load_stars(self, file_path):
+        stars = []
+        with open(file_path, 'r') as file:
+            for line in file:
+                # Parse each line of the bsc5.dat file
+                # The format is fixed-width, so we'll use string slicing
+                star_id = int(line[0:4])
+                ra_hours = float(line[75:77])
+                ra_minutes = float(line[77:79])
+                ra_seconds = float(line[79:83])
+                dec_degrees = float(line[84:86])
+                dec_minutes = float(line[86:88])
+                dec_seconds = float(line[88:90])
+                magnitude = float(line[102:107])
 
-    def add_star(self, name, ra, dec, magnitude):
-        self.cursor.execute('''
-            INSERT INTO stars (name, ra, dec, magnitude)
-            VALUES (?, ?, ?, ?)
-        ''', (name, ra, dec, magnitude))
-        self.conn.commit()
+                # Convert RA and Dec to decimal degrees
+                ra = (ra_hours + ra_minutes/60 + ra_seconds/3600) * 15  # 15 degrees per hour
+                dec = dec_degrees + dec_minutes/60 + dec_seconds/3600
+                if line[83] == '-':
+                    dec = -dec
 
-    def get_star(self, name):
-        self.cursor.execute('SELECT * FROM stars WHERE name = ?', (name,))
-        return self.cursor.fetchone()
+                stars.append((star_id, None, ra, dec, magnitude))
+        return stars
 
     def get_all_stars(self):
-        self.cursor.execute('SELECT * FROM stars')
-        return self.cursor.fetchall()
+        return self.stars
+
+    def get_star(self, star_id):
+        for star in self.stars:
+            if star[0] == star_id:
+                return star
+        return None
 
     def close(self):
         self.conn.close()
